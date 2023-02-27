77c7fc911dccfadb9d5010a4ac7037b2ec954c8e
chabrack <mohamed.saber35@gmail.com>
2023-02-27 21:38:13 +0100

    Revert "création infos accueil"
    This reverts commit 564977f6006aa548dcb8cf0231a6792836b2c205.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,14 +11,9 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        $prenom = 'imad';
-        $nom = 'ibnoulmalih';
-        $age = 25;
-        $profession = 'dev';
+
         return $this->render('home/index.html.twig', [
-            'prenom' => $prenom,
-            'nom' => $nom,
-            'age' => $age
+            'controller_name' => 'HomeController',
         ]);
     }
 }
