d611e94769bcd46f0c30fecb3151188a4aeed600
chabrack <mohamed.saber35@gmail.com>
2023-02-28 01:00:11 +0100

    Ajout entite entraineur et relation EN=>JO & EQ=>EN

--- src/Entity/Joueur.php
+++ src/Entity/Joueur.php
@@ -25,6 +25,9 @@ class Joueur
     #[ORM\Column(length: 255)]
     private ?string $poste = null;
 
+    #[ORM\ManyToOne(inversedBy: 'joueurs')]
+    private ?Entraineur $entraineur = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -74,6 +77,18 @@ class Joueur
     public function setPoste(string $poste): self
     {
         $this->poste = $poste;
+
+        return $this;
+    }
+
+    public function getEntraineur(): ?Entraineur
+    {
+        return $this->entraineur;
+    }
+
+    public function setEntraineur(?Entraineur $entraineur): self
+    {
+        $this->entraineur = $entraineur;
 
         return $this;
     }
