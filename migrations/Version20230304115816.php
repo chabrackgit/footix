0ea9319f7b118e367f4defd0ca996514ee442f98
chabrackgit <msaber.shi@outlook.com>
2023-03-04 14:05:02 +0100

    rendu & modifications migration

--- migrations/Version20230304115816.php
+++ migrations/Version20230304115816.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230303121646 extends AbstractMigration
+final class Version20230304115816 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20230303121646 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE terrain (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, couleur VARCHAR(255) NOT NULL, surface INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE equipe ADD drapeau VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE joueur ADD salaire VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE terrain');
         $this->addSql('ALTER TABLE equipe DROP drapeau');
+        $this->addSql('ALTER TABLE joueur DROP salaire');
     }
 }
