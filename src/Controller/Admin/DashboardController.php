addf28424ab1cac3e8790a0cd3326bbd5ef8c6ea
chabrackgit <msaber.shi@outlook.com>
2023-03-06 22:34:18 +0100

    ajout crud all - easyadmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller\Admin;
 
-use App\Controller\EquipeController;
+use App\Entity\User;
+use App\Entity\Equipe;
+use App\Entity\Joueur;
+use App\Entity\Terrain;
 use App\Entity\Entraineur;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
@@ -28,6 +30,10 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Entraineur', 'fas fa-list', Entraineur::class);
+        yield MenuItem::linkToCrud('Entraineur', 'fa-solid fa-user-tie', Entraineur::class);
+        yield MenuItem::linkToCrud('Equipe', 'fa fa-people-group', Equipe::class);
+        yield MenuItem::linkToCrud('Joueur', 'fa-solid fa-people-roof', Joueur::class);
+        yield MenuItem::linkToCrud('Terrain', 'fa-solid fa-futbol', Terrain::class);
+        yield MenuItem::linkToCrud('Utilisateur', 'fa-solid fa-user', User::class);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,16 +10,7 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
-    {
-        $prenom = 'imad';
-        $nom = 'ibnoulmalih';
-        $age = 25;
-        $profession = 'dev';
-        return $this->render('home/index.html.twig', [
-            'prenom' => $prenom,
-            'nom' => $nom,
-            'age' => $age,
-            'prof' => $profession
-        ]);
+    {   
+        return $this->render('home/index.html.twig');
     }
 }
